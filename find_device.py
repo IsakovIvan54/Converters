3e62981ed64cab633586f082a7746cd7cc3c6a13
Lab <lab@gosprodazy.local>
2023-07-19 18:09:01 +0300

    add fin device file

--- find_device.py
+++ find_device.py
@@ -6,28 +6,15 @@ rm = pyvisa.ResourceManager()
 # Get a list of available devices
 devices = rm.list_resources()
 
-# Create objects for each device
+
+
+print(devices)
+print("Количество подключенных приборов:" + str(len(devices)))
+print(devices[0])
+
 device_objects = {}
-for device in devices:
-    # Open the device
-    instrument = rm.open_resource(device)
-    
-    # Send the *IDN? command to get the device name
-    name = instrument.query('*IDN?')
-    
-    # Store the device object with its corresponding name
-    device_objects[name.strip()] = instrument
 
-# Example usage:
-# Access the device object by its name
-device_name = "DeviceName"
-if device_name in device_objects:
-    device = device_objects[device_name]
-    # Do something with the device object
-    device.write('Some SCPI command')
-else:
-    print(f"Device '{device_name}' not found")
-
-# Close all device connections
-for device in device_objects.values():
-    device.close()
+for i in range(0,len(devices)):
+    print("VISA: " + str(devices[i]))
+    instrument = rm.open_resource(devices[i])
+    print(instrument.query('*IDN?'))
